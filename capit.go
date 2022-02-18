651ad9b3a6c9171773eec015dc2ea6be6f822dff
Sander van Malssen <svm@kozmix.org>
2022-02-18 17:10:21 +0100

    *** empty log message ***

--- capit.go
+++ capit.go
@@ -1,22 +1,30 @@
 // capitalise from stdin to stdout
-// SvM 29-DEC-2020 - 02-FEB-2021
+// SvM 29-DEC-2020 - 15-JUL-2021
 //
 // nicked from https://gobyexample.com/line-filters
+// and from https://pkg.go.dev/golang.org/x/text@v0.3.6/cases
 
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	"os"
-	"strings"
 )
 
 func main() {
+	lng := "en"
+	flag.StringVar(&lng, "L", "en", "select language")
+	flag.Parse()
+	lngtag := language.Make(lng)
+	c := cases.Title(lngtag)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		// since strings.Title only works one way (lower to upper), lower everything first
-		fmt.Println(strings.Title(strings.ToLower(scanner.Text())))
+		fmt.Println(c.String(scanner.Text()))
 	}
 
 	if err := scanner.Err(); err != nil {
